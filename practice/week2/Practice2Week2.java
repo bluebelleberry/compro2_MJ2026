2d8e53ea0a484c24812825bb4bbe2840ef8f925d
meliscentaquino <meliscentjhoy.aquino@lorma.edu>
2026-02-19 22:09:21 +0800

    arrange file

---
+++ practice/week2/Practice2Week2.java
@@ -0,0 +1,17 @@
+public class Practice2Week2 {
+    public static void main(String[] args) {
+        int[] theaterRow = {0,0,0,0,0,0,0};
+        theaterRow[3] = 1;
+
+        for (int i = 0; i < theaterRow.length; i++) {
+            
+            if (theaterRow[i] == 0){
+                System.out.printf("Seat %d: Available\n", i+1);
+            } else {
+                System.out.printf("Seat %d: Booked\n", i+1);
+            }             
+        }System.out.println();   
+    }
+
+}
+
