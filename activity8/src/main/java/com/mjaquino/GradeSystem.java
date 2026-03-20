eeaaf920e6adc7ab23ede0ce0959982b3a0583be
meliscentaquino <meliscentjhoy.aquino@lorma.edu>
2026-03-20 22:53:29 +0800

    activity 8

---
+++ activity8/src/main/java/com/mjaquino/GradeSystem.java
@@ -0,0 +1,120 @@
+package com.mjaquino;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class GradeSystem {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String[] subjects = {
+                "COMPRO2",
+                "OOP",
+                "DATSTRU",
+                "MMW",
+                "UTS"
+        };
+
+        int[][] grades = new int[5][3];
+
+        boolean mainLoop = true;
+
+        while (mainLoop) {
+
+            System.out.println("""
+                    
+                    Main Menu
+                    [ 1 ] Enter Grades
+                    [ 2 ] Display Grades
+                    [ 3 ] Exit
+                    """);
+
+            System.out.print("Enter your choice: ");
+            int menuChoice = input.nextInt();
+
+            if (menuChoice == 1) {
+
+                boolean subjectGrades = true;
+
+                while (subjectGrades) {
+
+                    System.out.println("""
+                            
+                            Enter Grades
+                            [ 1 ] COMPRO2
+                            [ 2 ] OOP
+                            [ 3 ] DATSTRU
+                            [ 4 ] MMW
+                            [ 5 ] UTS
+                            [ 0 ] Back
+                            """);
+
+                    System.out.print("Enter your choice: ");
+                    int subjectChoice = input.nextInt();
+
+                    if (subjectChoice == 0) {
+                        subjectGrades = false;
+                    }
+
+                    else if (subjectChoice >= 1 && subjectChoice <= 5) {
+
+                        int subjectIndex = subjectChoice - 1;
+
+                        System.out.println("\nEnter grades for " + subjects[subjectIndex]);
+
+                        System.out.print("Prelim: ");
+                        grades[subjectIndex][0] = input.nextInt();
+
+                        System.out.print("Midterm: ");
+                        grades[subjectIndex][1] = input.nextInt();
+
+                        System.out.print("Final: ");
+                        grades[subjectIndex][2] = input.nextInt();
+
+                        System.out.println("Grades saved!\n");
+
+                        
+                    }
+
+                    else {
+                        System.out.println("Invalid choice\n");
+                    }
+                }
+
+            }
+
+            else if (menuChoice == 2) {
+
+                System.out.println("""
+                        
+                        Subject   Pre   Mid   Fin
+                        """);
+
+                for (int i = 0; i < subjects.length; i++) {
+                    System.out.printf("%-8s %3d %5d %5d%n",
+                            subjects[i],
+                            grades[i][0],
+                            grades[i][1],
+                            grades[i][2]);
+                }
+
+                System.out.println();
+            }
+
+            else if (menuChoice == 3) {
+                System.out.println("Exiting program...");
+                mainLoop = false;
+            }
+
+            else {
+                System.out.println("Invalid choice\n");
+            }
+        }
+    }
+
+
+}
