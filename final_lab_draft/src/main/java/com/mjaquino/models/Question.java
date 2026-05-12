9be16f1ffb58b3dcc627844aff39a2cfd792ad0e
MJ <meliscentjhoy.aquino@lorma.edu>
2026-05-12 08:55:46 +0800

    Update Question.java

--- final_lab_draft/src/main/java/com/mjaquino/models/Question.java
+++ final_lab_draft/src/main/java/com/mjaquino/models/Question.java
@@ -1,5 +1,34 @@
 package com.mjaquino.models;
 
 public abstract class Question {
-    
+    // data feilds
+    protected String questionText;
+    protected String type;
+    protected int timeLimit;
+
+    // constructor
+    public Question(String questionText,
+                    String type,
+                    int timeLimit) {
+
+        this.questionText = questionText;
+        this.type = type;
+        this.timeLimit = timeLimit;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public abstract void displayQuestion();
+
+    public abstract boolean checkAnswer(String userAnswer);
 }
